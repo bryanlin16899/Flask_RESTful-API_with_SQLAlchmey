6d5e1d46e15df2874fa822c20cf26b0d575affc8
Bryan Lin <78909016+bryanlin16899@users.noreply.github.com>
2021-10-15 00:23:39 +0800

    Add PostgreSQL environment variable.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 from flask import Flask
 from flask.wrappers import Request
@@ -12,7 +13,10 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('uri', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'look'
 api = Api(app)
